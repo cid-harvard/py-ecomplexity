f3538da9344badee8aff45d6bceb7e8a7146b6da
Shreyas Gadgin Matha <shreyas.gm61@gmail.com>
2018-12-19 23:36:11 -0500

    Function to calculate proximity set up

---
+++ ecomplexity/proximity.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+def calc_proximity(rca_or_mcp, ubiquity, continuous=False, asymmetric=False):
+    """Calculate product proximity matrices
+
+    Args:
+        rca_or_mcp: numpy ndarray with rows as locations and columns as products.
+            Considered to be RCA if continuous is True. Else considered as Mcp.
+        ubiquity: numpy array of shape=number of columns in "rca_or_mcp"
+        continuous: Whether to consider correlation of every product pair (True)
+            or product co-occurrence (False). *default* False.
+        asymmetric: Whether to generate asymmetric proximity matrix (True) or
+            symmetric (False). *default* False.
+
+    Returns:
+        pandas df with proximity values for every product pair
+    """
+    if continuous==False:
+        # Calculate discrete proximity
+        mcp = rca_or_mcp
+        phi = mcp.T @ mcp
+        phi = (phi / ubiquity[np.newaxis, :])
+
+        if asymmetric==False:
+            # Symmetric proximity matrix
+            phi = np.minimum(phi, phi.T) - np.identity(mcp.shape[1])
+        elif asymmetric==True:
+            # Asymmetric proximity matrix
+            phi = phi.T - np.identity(mcp.shape[1])
+
+    elif continuous==True:
+        # Calculate continuous proximity
+        rca = rca_or_mcp
+        phi = (1 + np.corrcoef(rca.T))/2 - np.identity(rca.shape[1])
+
+    return(phi)
+
+def proximity(data, rca_or_mcp, ubiquity, continuous=False, asymmetric=False):
+    """Wrapper function to calculate product proximity matrices
+
+    Args:
+        data: pandas df with cols 'time','loc','prod','val'
+        rca_or_mcp: numpy ndarray with rows as locations and columns as products.
+            Considered to be RCA if continuous is True. Else considered as Mcp.
+        ubiquity: numpy array of shape=number of columns in "rca_or_mcp"
+        continuous: Whether to consider correlation of every product pair (True)
+            or product co-occurrence (False). *default* False.
+        asymmetric: Whether to generate asymmetric proximity matrix (True) or
+            symmetric (False). *default* False.
+
+    Returns:
+        pandas df with proximity values for every product pair
+    """
+    # Reshape as df
+    output_index = pd.MultiIndex.from_product([data.index.levels[1], data.index.levels[1]])
+    output = pd.DataFrame(data={'proximity':phi.ravel()},
+                          index=output_index)
+    output.head()
