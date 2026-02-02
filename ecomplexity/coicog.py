2ecab65a99441687fcd9b0c03c17521b44b35e60
Elise Jackson <ejackson@holy8a26502.rc.fas.harvard.edu>
2026-02-02 16:13:24 -0500

    pdb insert

--- ecomplexity/coicog.py
+++ ecomplexity/coicog.py
@@ -13,6 +13,7 @@ def calc_coi_cog(cdata, proximity_mat):
         cata: ComplexityData object with attribute coi
     """
     # mata coi = ((density:*(1 :- M)):*kp)*J(Npx,Npx,1)
+    import pdb; pdb.set_trace()
     coi = ((cdata.density_t * (1 - cdata.mcp_t)) * cdata.pci_t).sum(axis=1)
     # print(coi.shape)
     # mata cog = (1 :- M):*((1 :- M) * (proximity :* ((kp1d:/(proximity*J(Npx,1,1)))*J(1,Npx,1))))
